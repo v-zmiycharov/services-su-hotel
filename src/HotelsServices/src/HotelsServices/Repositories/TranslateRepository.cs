5ce1113d188fbde5422192d2b69515f7a0f9b043
Valentin Zmiycharov <valentin.zmiycharov@gmail.com>
2016-04-05 10:20:11 +0300

    Fix yandex translate to be sync

--- src/HotelsServices/src/HotelsServices/Repositories/TranslateRepository.cs
+++ src/HotelsServices/src/HotelsServices/Repositories/TranslateRepository.cs
@@ -41,47 +41,32 @@ namespace HotelsServices.Repositories
 
         public string Translate(string input, string toLang, string fromLang)
         {
-            string url = string.Format("https://translate.yandex.net/api/v1.5/tr.json/translate?key={0}&text={1}&lang={2}-{3}"
-                , "trnsl.1.1.20160329T125742Z.7cf7029a7299121f.e2035c38942a41a74d94459866c16182668762f8"
-                , input
-                , fromLang
-                , toLang);
+            YandexTranslateResult yandexResult = null;
 
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://translate.yandex.net/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-            var result = GetRequest<JObject>(url).ToObject<YandexTranslateResult>();
-
-            if (result != null && result.Text != null && result.Text.Count > 0)
-                return result.Text[0];
+                var response = client.GetAsync(string.Format("api/v1.5/tr.json/translate?key={0}&text={1}&lang={2}-{3}"
+                    , "trnsl.1.1.20160329T125742Z.7cf7029a7299121f.e2035c38942a41a74d94459866c16182668762f8"
+                    , input
+                    , fromLang
+                    , toLang))
+                .GetAwaiter().GetResult();
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonContent = response.Content.ReadAsStringAsync().Result;
+                    yandexResult = JsonConvert.DeserializeObject<YandexTranslateResult>(jsonContent);
+                }
+            }
+            
+            if (yandexResult != null && yandexResult.Text != null && yandexResult.Text.Count > 0)
+                return yandexResult.Text[0];
             else
                 return input;
         }
-
-
-        private static T GetRequest<T>(string url)
-        {
-            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
-
-            request.Method = "GET";
-            request.AllowAutoRedirect = false;
-            request.Accept = "application/json, text/plain, */*";
-            request.ContentType = "application/json; charset=UTF-8";
-
-            return ReadResponse<T>(request);
-        }
-
-        private static T ReadResponse<T>(HttpWebRequest request)
-        {
-            T jO;
-
-            using (var response = request.GetResponse())
-            using (var responseStream = response.GetResponseStream())
-            using (var reader = new StreamReader(responseStream))
-            {
-                var responseFromServer = reader.ReadToEnd();
-                jO = JsonConvert.DeserializeObject<T>(responseFromServer);
-            }
-
-            return jO;
-        }
+        
     }
 }
