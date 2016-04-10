4653d21d874bd7054cd0bf4d973f7626a978a080
Vladislav Belichev <vbelichev@gmail.com>
2016-04-10 02:44:02 -0700

    Hotels schema updated, method bugs fixed and data moved to another static class

--- soa-java-soap/src/main/java/com/sources/hotels/Facilities.java
+++ soa-java-soap/src/main/java/com/sources/hotels/Facilities.java
@@ -17,16 +17,16 @@ import javax.xml.bind.annotation.XmlType;
 
 
 /**
- * <p>Java class for hotels complex type.
+ * <p>Java class for facilities complex type.
  * 
  * <p>The following schema fragment specifies the expected content contained within this class.
  * 
  * <pre>
- * &lt;complexType name="hotels">
+ * &lt;complexType name="facilities">
  *   &lt;complexContent>
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
- *         &lt;element name="hotel" type="{http://hotels.sources.com}hotel1" maxOccurs="unbounded"/>
+ *         &lt;element name="facility" type="{http://www.w3.org/2001/XMLSchema}string" maxOccurs="20"/>
  *       &lt;/sequence>
  *     &lt;/restriction>
  *   &lt;/complexContent>
@@ -36,41 +36,41 @@ import javax.xml.bind.annotation.XmlType;
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "hotels", propOrder = {
-    "hotel"
+@XmlType(name = "facilities", propOrder = {
+    "facility"
 })
-public class Hotels {
+public class Facilities {
 
     @XmlElement(required = true)
-    protected List<Hotel1> hotel;
+    protected List<String> facility;
 
     /**
-     * Gets the value of the hotel property.
+     * Gets the value of the facility property.
      * 
      * <p>
      * This accessor method returns a reference to the live list,
      * not a snapshot. Therefore any modification you make to the
      * returned list will be present inside the JAXB object.
-     * This is why there is not a <CODE>set</CODE> method for the hotel property.
+     * This is why there is not a <CODE>set</CODE> method for the facility property.
      * 
      * <p>
      * For example, to add a new item, do as follows:
      * <pre>
-     *    getHotel().add(newItem);
+     *    getFacility().add(newItem);
      * </pre>
      * 
      * 
      * <p>
      * Objects of the following type(s) are allowed in the list
-     * {@link Hotel1 }
+     * {@link String }
      * 
      * 
      */
-    public List<Hotel1> getHotel() {
-        if (hotel == null) {
-            hotel = new ArrayList<Hotel1>();
+    public List<String> getFacility() {
+        if (facility == null) {
+            facility = new ArrayList<String>();
         }
-        return this.hotel;
+        return this.facility;
     }
 
 }
